0a3cc8a7a371b9aa8f3dfcc08427484c007fb89e
Bair Dev <trial.and.error@gmx.net>
2017-08-11 13:27:02 +0200

    using .default for default exports

--- src/js/module2.js
+++ src/js/module2.js
@@ -1,15 +1,15 @@
 define(
     ['./module3'],
     function(Something){
-        console.log('Inside module2, what type is the exported constructor of module3?', typeof Something, Something);
+        console.log('Inside module2, what type is the exported constructor of module3?', typeof Something, Something, Something.default);
         // TODO: put this on the page
 
         // 1. test
-        var something = new Something();
+        var something = new Something.default();
         console.log('check instance of something', something.sayHello('June'));
 
         return function Anotherthing(){
-            this.something = new Something();
+            this.something = new Something.default();
         };
     }
 );
